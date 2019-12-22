3dbe816846f527e9989987b1c7e3a5a9ac757701
Itbd <58792045+okbd@users.noreply.github.com>
2019-12-22 14:16:08 +0600

    Add files via upload

---
+++ Peactv/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    var player = videojs('video');
+
+    player.playlist([
+        {
+            name: 'Dsports',
+            autoplay: true,
+            sources: [
+                {src: 'http://172.28.28.112/live/antbd1001/index.m3u8', type: 'application/x-mpegURL'},
+            ]
+        },
+        {
+            name: 'Gazi TV',
+            sources: [
+                {src: 'http://172.28.28.112/live/antbd1006/index.m3u8', type: 'application/x-mpegURL'},
+            ]
+        },
+        {
+            name: 'Masranga Tv',
+            sources: [
+                {src: 'http://103.85.196.10:8080/hls/one.m3u8', type: 'application/x-mpegURL'},
+            ]
+        },
+        {
+            name: 'Masranga Tv 2',
+            sources: [
+                {src: 'http://boss.bdtv.live:8080/hls/2.m3u8', type: 'application/x-mpegURL'},
+            ]
+		},
+		{
+            name: 'Masranga 2',
+            sources: [
+                {src: 'http://boss.bdtv.live:8080/hls/3.m3u8', type: 'application/x-mpegURL'},
+            ]
+        },
+		{
+            name: 'sony six',
+            sources: [
+                {src: 'http://172.28.28.112/live/antbd1002/index.m3u8', type: 'application/x-mpegURL'},
+            ]
+        }]);
+
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+    player.playlistUi();
+});
